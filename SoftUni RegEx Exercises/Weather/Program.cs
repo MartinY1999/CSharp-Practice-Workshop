f9739b7526944feba773624dfb2465a638a535e0
MartinY1999 <65725924+MartinY1999@users.noreply.github.com>
2020-06-02 20:07:52 +0300

    Added Exercises

---
+++ SoftUni RegEx Exercises/Weather/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Weather
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Weather> weathers = new List<Weather>();
+            string pattern = @"([A-Z]{2})(\d+\.\d+)([A-Za-z]+)\|";
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "end") break;
+                else
+                {
+                    Match result = Regex.Match(input, pattern);
+                    if (result.Success)
+                    {
+                        string name = result.Groups[1].Value;
+                        string averageTemp = result.Groups[2].Value;
+                        string type = result.Groups[3].Value;
+                        if (weathers.Any(x => x.Name == name) == false)
+                        {
+                            Weather newW = new Weather(name, double.Parse(averageTemp), type);
+                            weathers.Add(newW);
+                        }
+                        else
+                        {
+                            int index = weathers.FindIndex(x => x.Name == name);
+                            if (weathers[index].Temperature == double.Parse(averageTemp))
+                            {
+                                if (weathers[index].Type == type) continue;
+                                else weathers[index].Type = type;
+                            }
+                            else
+                            {
+                                weathers[index].Temperature = double.Parse(averageTemp);
+                                weathers[index].Type = type;
+                            }
+                        }
+                    }
+                }
+            }
+            foreach (Weather w in weathers.OrderBy(x => x.Temperature))
+            {
+                Console.WriteLine($"{w.Name} => {w.Temperature:F2} => {w.Type}");
+            }
+        }
+    }
+    class Weather
+    {
+        public string Name { get; set; }
+        public double Temperature { get; set; }
+        public string Type { get; set; }
+        public Weather(string n, double temp, string type)
+        {
+            Name = n;
+            Temperature = temp;
+            Type = type;
+        }
+    }
+}
---
+++ SoftUni RegEx Exercises/WordEncounter/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WordEncounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filter = Console.ReadLine();
+            char character = filter[0];
+            int digit = Convert.ToInt32(Convert.ToString(filter[1]));
+            string pattern = @"[A-Z].*?(\.|\!|\?)$";
+            List<string> words = new List<string>();
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "end") break;
+                else
+                {
+                    Match sentence = Regex.Match(input, pattern);
+                    if (sentence.Success)
+                    {
+                        MatchCollection wrds = Regex.Matches(sentence.Value, @"\w+");
+                        foreach (Match wrd in wrds)
+                        {
+                            char[] letters = wrd.Value.ToCharArray();
+                            int counter = 0;
+                            foreach (char l in letters)
+                            {
+                                if (l == character) counter++;
+                            }
+                            if (counter >= digit) words.Add(wrd.Value);
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(String.Join(", ", words));
+            Console.ReadLine();
+        }
+    }
+}
